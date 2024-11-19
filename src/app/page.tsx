5ceae590c470eeb7e6fe9960035e62eaa4291d50
Bisma Muqeed <bismamuqeed@gmail.com>
2024-11-20 02:51:48 +0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Hero2/>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default page
